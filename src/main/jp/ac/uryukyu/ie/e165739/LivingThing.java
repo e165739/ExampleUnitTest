2ea964d59e5ad58216d21de738feed0ad9634e9d
Asayama Ibuki <e165739@ie.u-ryukyu.ac.jp>
2016-11-21 12:40:45 +0900

    1st example

--- src/main/jp/ac/uryukyu/ie/e165739/LivingThing.java
+++ src/main/jp/ac/uryukyu/ie/e165739/LivingThing.java
@@ -1,47 +1,57 @@
 package jp.ac.uryukyu.ie.e165739;
 
 public class LivingThing {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
-    /**
-     * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name ヒーロー名
-     * @param maximumHP ヒーローのHP
-     * @param attack ヒーローの攻撃力
-     */
-    public LivingThing (String name, int maximumHP, int attack) {
+    public LivingThing (String name, int hitPoint, int attack) {
         this.name = name;
-        hitPoint = maximumHP;
+        this.hitPoint = hitPoint;
         this.attack = attack;
         dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, hitPoint, attack);
     }
 
     /**
      * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
      * @return boolean
      */
+
+    public void setDead(boolean dead){
+        this.dead = dead;
+    }
     public boolean isDead(){
         return  dead;
     }
 
+    public void setName(String name){
+        this.name = name;
+    }
     public String getName(){
         return name;
     }
 
+    public void setHitPoint(int hitPoint){ this.hitPoint = hitPoint; }
+    public int getHitPoint(){
+        return hitPoint;
+    }
+
+    public void setAttack(int attack){ hitPoint = attack;}
+    public int getAttack(){
+        return attack;
+    }
     /**
      * Enemyへ攻撃するメソッド。
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param e 攻撃対象
+     * @param opponent 攻撃対象
      */
-    public void attack(LivingThing){
+    public void attack(LivingThing opponent){
         if( hitPoint > 0 ) {
             int damage = (int) (Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
-            LivingThing.wounded(damage);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
         }
     }
 
@@ -53,6 +63,7 @@ public class LivingThing {
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint <= 0 ) {
-        dead = true;
+            dead = true;
+        }
     }
 }
